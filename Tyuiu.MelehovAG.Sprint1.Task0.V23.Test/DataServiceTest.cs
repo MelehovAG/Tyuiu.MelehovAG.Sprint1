849881b93eaeb694781f8e366e961547ee5e9520
Tyuiu.MelehovAG@outlook.com <dedmeleh@gmail.com>
2023-10-17 00:28:34 +0500

    Task0

---
+++ Tyuiu.MelehovAG.Sprint1.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.MelehovAG.Sprint1.Task0.V23.Lib;
+
+namespace Tyuiu.MelehovAG.Sprint1.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(13, res);
+        }
+    }
+}
---
+++ Tyuiu.MelehovAG.Sprint1.Task0.V23/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MelehovAG.Sprint1.Task0.V23.Lib;
+
+//ЗАДАНИЕ
+//Написать программу, которая вычисляет выражение (15/5*4)+1 и печатает результат на экране.
+//Название проектов (консольного приложения, библиотеки классов, тестового модуля) оформить по шаблону в соответствии с вариантом. Интерфейс консольного приложения оформить по шаблону. 
+
+namespace Tyuiu.MelehovAG.Sprint1.Task0.V23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Мелехов А.Г. | ПКТб-23-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнил: Мелехов Алексей Григорьевич | ПКТб-23-1                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение (15 / 5 * 4) + 1 и      *");
+            Console.WriteLine("* печатает результат на экране.                                           *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* (15 / 5 * 4) + 1                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            //Метод Calculate находится в библиотеке Tyuiu.SpirinIS.Sprint1.Task0.V23.Lib
+            //в классе DataService
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
